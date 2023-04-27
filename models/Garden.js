38d76c35680266b74c976414560b4fc8f563e4a6
McNeill Shiner <mcneill.shiner@gmail.com>
2023-04-27 08:29:17 -0400

    Added garden schema

---
+++ models/Garden.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const GardenSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  zip: {
+    type: Number,
+    require: true,
+  },
+  firstFrost: {
+    type: Date,
+    require: true,
+  },
+  lastFrost: {
+    type: Date,
+    require: true,
+  },
+  gardenMethod: {
+    [type: String]
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
